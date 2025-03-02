c12295809e5260a6b3292cef07203afbe317617a
ZingZing_ <rzha972@aucklanduni.ac.nz>
2025-03-02 13:42:02 +1300

    Add EJS view engine and homepage template

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
 app.get("/", (req, res) => {
-  res.send("Hello World from our server???")
+  res.render("homepage")
 })
 
 app.listen(3000)
