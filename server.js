6005c48f96b36988d64f5b8d2d34c7b7fba87be2
ZingZing_ <rzha972@aucklanduni.ac.nz>
2025-03-03 15:54:02 +1300

    Add user registration with password hashing and SQLite setup

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
+const bcrypt = require("bcrypt")
 const express = require('express')
+const db = require("better-sqlite3")("database.db")
+// Set the journal mode to WAL (Write-Ahead Logging) for better concurrency and performance
+db.pragma("journal_mode = WAL")
+
+// Database schema
+const createTables = db.transaction(() => {
+  db.prepare(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username STRING NOT NULL UNIQUE,
+      password STRING NOT NULL)
+    `).run()
+})
+
+createTables()
+
 const app = express()
 
 app.set('view engine', 'ejs')
@@ -39,9 +56,15 @@ app.post("/register", (req, res) => {
   }
 
   // save the new user into a database
+  const salt = bcrypt.genSaltSync(10)
+  req.body.password = bcrypt.hashSync(req.body.password, salt)
+
+  const ourStatement = db.prepare("INSERT INTO users (username, password) VALUES (?,?)")
+  ourStatement.run(req.body.username, req.body.password)
 
 
   // log the user in by giving them a cookie
+  res.send("You have been registered")
 })
 
 app.listen(3000)
